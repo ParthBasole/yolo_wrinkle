54ad872d61b0b4d3d8ab1ee178e429ca08d92ae5
ParthB <pbasole@gmail.com>
2024-06-03 08:59:21 +0530

    print

--- seg/wrinkle_detector.py
+++ seg/wrinkle_detector.py
@@ -4,13 +4,14 @@ from detect import parse_opt, run
 class WrinkleDetector:
     @staticmethod
     def get_wrinkles(source, weights_path,name):
-        parser = argparse.ArgumentParser(description='Wrinkle Detection Tool')
         print(weights_path)
         print(source)
         print(name)
+        parser = argparse.ArgumentParser(description='Wrinkle Detection Tool')
         args = parser.parse_args()
+        print(**vars(args))
         opt = parse_opt()
-        run(**vars(opt))
+        run(source=source, weights=weights_path,name=name)
 
 def main():
     parser = argparse.ArgumentParser(description='Wrinkle Detection Tool')
@@ -18,7 +19,7 @@ def main():
     parser.add_argument('--weights_path', type=str, help='Path to the weights file for the detection model')
     parser.add_argument('--name', type=str, help='Model Name')
 
-    args = parser.parse_args()
+
 
     WrinkleDetector.get_wrinkles(args.source, args.weights_path,args.name)
 
